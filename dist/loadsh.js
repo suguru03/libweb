110685898574baf0caca9aafafb577cf637fd6b3
Suguru Motegi <suguru.motegi@gmail.com>
2017-10-19 10:23:21 -0700

    Update gulp script and lodash

--- dist/loadsh.js
+++ dist/loadsh.js
@@ -768,6 +768,4 @@ JSON5.stringify = function (obj, replacer, space) {
     }
     return internalStringify(topLevelHolder, '', true);
 };
-;if (typeof window === 'object') window.JSON5 = JSON5;function stringify(obj, replacer = null, space = 2) {
-  return JSON5.stringify(obj, replacer, space).replace(/"/g, '\'');
-}
+;if (typeof window === 'object') window._ = _;
--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,20 @@ gulp.task('dist', () => {
 });
 
 function exposeLodash() {
-  const filepath = path.resolve(__dirname, 'node_modules/lodash/lodash.min.js');
-  const file = fs.readFileSync(filepath, 'utf8');
-  const targetpath = path.resolve(__dirname, 'dist/lodash.min.js');
-  fs.writeFileSync(targetpath, file, 'utf8');
+  expose('_', 'loadsh', 'json5/lib/json5.js');
 }
 
 function exposeJSON5() {
-  const filepath = path.resolve(__dirname, 'node_modules/json5/lib/json5.js');
-  const file = fs.readFileSync(filepath, 'utf8') +
-    'if (typeof window === \'object\') window.JSON5 = JSON5;' +
-    stringify.toString();
-  const targetpath = path.resolve(__dirname, 'dist/json5.js');
+  expose('JSON5', 'json5', 'json5/lib/json5.js', stringify.toString());
+}
+
+function expose(funcname, filename, filepath, extra = '') {
+  const libpath = path.resolve(__dirname, `node_modules/${filepath}`);
+  const file = `${fs.readFileSync(libpath, 'utf8')};if (typeof window === 'object') window.${funcname} = ${funcname};${extra}`;
+  const targetpath = path.resolve(__dirname, `dist/${filename}.js`);
   fs.writeFileSync(targetpath, file, 'utf8');
 }
 
 function stringify(obj, replacer = null, space = 2) {
-  return JSON5.stringify(obj, replacer, space).replace(/"/g, "'");
+  return JSON5.stringify(obj, replacer, space).replace(/"/g, '\'');
 }
