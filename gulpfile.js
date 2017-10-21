f8420c497dc4d56eb71a0dc93f97fe3ceb1ff572
Suguru Motegi <suguru.motegi@gmail.com>
2017-10-21 13:46:54 -0700

    Add aigle, async, neo-async, bluebird

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,59 @@
 const fs = require('fs');
 const path = require('path');
 
+const _ = require('lodash');
 const gulp = require('gulp');
 
+const lib = {
+  json5: {
+    name: 'JSON5',
+    path: 'json5/lib/json5.js',
+    after: stringify.toString()
+  },
+  lodash: {
+    name: '_',
+    path: 'lodash/lodash.min.js'
+  },
+  async: {
+    name: 'async',
+    path: 'async/dist/async.min.js'
+  },
+  'neo-async': {
+    name: 'neo_async',
+    path: 'neo-async/async.min.js'
+  },
+  bluebird: {
+    name: 'Bluebird',
+    path: 'bluebird/js/browser/bluebird.min.js',
+    before: 'var exports = window, module = { exports: exports };',
+    after: 'window.Bluebird = module.exports;'
+  },
+  aigle: {
+    name: 'Aigle',
+    path: 'aigle/aigle-es5.min.js',
+    before: 'var exports = window, module = { exports: exports };',
+    after: 'window.Aigle = module.exports;'
+  }
+};
+
 gulp.task('dist', () => {
-  exposeLodash();
-  exposeJSON5();
+  _.forOwn(lib, ({ name, path, before, after }, key) => expose(name, key, path, before, after));
+  createHTML();
 });
 
-function exposeLodash() {
-  expose('_', 'lodash', 'lodash/lodash.min.js');
-}
-
-function exposeJSON5() {
-  expose('JSON5', 'json5', 'json5/lib/json5.js', stringify.toString());
-}
-
-function expose(funcname, filename, filepath, extra = '') {
+function expose(funcname, filename, filepath, before = '', after = '') {
   const libpath = path.resolve(__dirname, `node_modules/${filepath}`);
-  const file = `${fs.readFileSync(libpath, 'utf8')};if (typeof window === 'object') window.${funcname} = ${funcname};${extra}`;
+  const file = `${before}${fs.readFileSync(libpath, 'utf8')};${after}`;
   const targetpath = path.resolve(__dirname, `dist/${filename}.js`);
   fs.writeFileSync(targetpath, file, 'utf8');
 }
 
 function stringify(obj, replacer = null, space = 2) {
   return JSON5.stringify(obj, replacer, space).replace(/"/g, '\'');
+}
+
+function createHTML() {
+  const html = _.reduce(lib, (html, v, key) => html + `<script src="dist/${key}.js"></script>\n`, '');
+  const filepath = path.resolve(__dirname, 'index.html');
+  fs.writeFileSync(filepath, html, 'utf8');
 }
